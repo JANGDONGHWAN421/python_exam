25d7504325fe47e52b4e676fbefcea86aa4c3928
JANGDONGHWAN421 <57391358+JANGDONGHWAN421@users.noreply.github.com>
2021-04-27 16:31:30 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from maxmin import *
+from sumall import *
+from myabs import *
+
+a = [1,3,4,5,6,79,-10,21,13,-24,5,78]
+
+print("sum = ", sumall(a))
+
+
+a = [1,3,4,5,6,7,-9,10,21,13,24,5,-78]
+
+print("max = ", maxi(a))
+
+a = [-1,3,4,5,6,-79,10,-21,13,24,5,-78]
+
+print("min = ", mini(a))
+
+myabs(a)
+
+# 20210427 _ 수업중 _ 초보니까 뭐라하지마.....
