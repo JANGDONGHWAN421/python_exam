1e7cff764bb068782ec1b96aebe7c19cd7edee8e
JANGDONGHWAN421 <57391358+JANGDONGHWAN421@users.noreply.github.com>
2021-04-27 16:32:20 +0900

    Create maxmin.py

---
+++ maxmin.py
@@ -0,0 +1,13 @@
+def maxi(m_list): # max 하자.
+    temp = m_list[0]
+    for (i,x) in enumerate(m_list):
+        if temp <= m_list[i]:
+            temp = m_list[i]
+    return temp
+
+def mini(m_list): # min  
+    temp = m_list[0]
+    for (i,x) in enumerate(m_list):
+        if temp >= m_list[i]:
+            temp = m_list[i]
+    return temp
